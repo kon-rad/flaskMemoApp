02935b3846a1d6b3a5bbca2f764994fb2d4186c3
konradgnat <konradmgnat@gmail.com>
2018-05-01 22:27:53 -0500

    init app

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='flaskFinanceApp',
+    packages=['flaskFinanceApp'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
